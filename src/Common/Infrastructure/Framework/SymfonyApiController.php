8129fd33c2f2549c2cc44d33d46038b257d1896d
Jcoca <jcoca@factorenergia.com>
2024-03-21 14:36:23 +0100

    agregamos -> servicio para validar el token al hacer una atentificacion

--- src/Common/Infrastructure/Framework/SymfonyApiController.php
+++ src/Common/Infrastructure/Framework/SymfonyApiController.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace App\Common\Infrastructure\Framework;
 
+use App\Auth\ApplicationService\Authenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 abstract class SymfonyApiController extends AbstractController
 {
+    public function __construct(private readonly Authenticator $authenticator)
+    {
+    }
+    final protected function tokenValidator(Request $request): void
+    {
+        $token = $request->headers->get('Authorization');
+        ($this->authenticator)($token);
+    }
 }
