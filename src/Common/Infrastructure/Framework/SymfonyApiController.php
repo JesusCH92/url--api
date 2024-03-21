558d356aebff4ed2771915b41ea2639b13cb8092
Jcoca <jcoca@factorenergia.com>
2024-03-21 14:18:49 +0100

    implementamos -> componente para la capa de las apis

---
+++ src/Common/Infrastructure/Framework/SymfonyApiController.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\Infrastructure\Framework;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+abstract class SymfonyApiController extends AbstractController
+{
+}
