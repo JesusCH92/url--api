d7d97c46ec2811b87d07adedff70faa5685730f6
Jcoca <jcoca@factorenergia.com>
2024-03-21 14:47:22 +0100

    agregamos -> documentacion de la api

--- src/Url/Infrastructure/Api/UrlShorterController.php
+++ src/Url/Infrastructure/Api/UrlShorterController.php
@@ -8,6 +8,7 @@ use App\Auth\ApplicationService\Authenticator;
 use App\Common\Infrastructure\Framework\SymfonyApiController;
 use App\Url\ApplicationService\DTO\UrlShorterRequest;
 use App\Url\ApplicationService\UrlShorter;
+use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,6 +22,47 @@ final class UrlShorterController extends SymfonyApiController
     }
 
     #[Route('/api/v1/shorts-urls', name: 'app_shorts_urls', methods: 'POST')]
+    #[OA\Post(
+        path: "/api/v1/shorts-urls",
+        summary: "Shorten a URL",
+        requestBody: new OA\RequestBody(
+            content: new OA\MediaType(
+                mediaType: "application/json",
+                schema: new OA\Schema(
+                    required: ["url"],
+                    properties: [
+                        new OA\Property(
+                            property: "url",
+                            description: "The URL to be shortened",
+                            type: "string",
+                            example: "https://symfony.com/"
+                        )
+                    ],
+                    type: "object"
+                )
+            )
+        ),
+        tags: ["URL"],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: "Successful operation",
+                content: new OA\MediaType(
+                    mediaType: "application/json",
+                    schema: new OA\Schema(
+                        properties: [
+                            new OA\Property(
+                                property: "url",
+                                description: "The shortened URL",
+                                type: "string"
+                            )
+                        ],
+                        type: "object"
+                    )
+                )
+            )
+        ]
+    )]
     public function shortUrls(Request $request): JsonResponse
     {
         $this->tokenValidator($request);
